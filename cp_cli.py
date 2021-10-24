fd2ac37adc95cd695b88ef16875e5bd3d2cf619b
Marco Merlin <marcomerli@gmail.com>
2021-10-24 02:03:19 +0200

    renamed xml to svd

--- cp_cli.py
+++ cp_cli.py
@@ -9,7 +9,7 @@ import sys
 
 def cp_cli(args=[]):
     parser = argparse.ArgumentParser(description='Cheap Pie Configuration')
-    parser.add_argument("-rf", "--regfname", help="register description file name", action='store', type = str, default="QN908XC.xml")
+    parser.add_argument("-rf", "--regfname", help="register description file name", action='store', type = str, default="QN908XC.svd")
     parser.add_argument("-dd", "--devicedir", help="register description files folder", action='store', type = str, default="./devices")
     parser.add_argument("-jd", "--jdevice", help="jlink device name", action='store', type = str, default="CORTEX-M4")
     parser.add_argument("-t" , "--transport", help="transport", action='store', type = str, default="jlink", choices=["jlink","dummy"])
