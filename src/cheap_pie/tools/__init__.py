2712be105003d61e4aad5300c0f6102f2048f4ed
Marco Merlin <marco.merlin@ceva-dsp.com>
2023-05-12 10:56:53 +0200

    fix import of cp_hal when install with pip

--- src/cheap_pie/tools/__init__.py
+++ src/cheap_pie/tools/__init__.py
@@ -1,2 +1,7 @@
-import tools.hal2doc
-import tools.search
+try:
+    # cheap_pie installed
+    import cheap_pie.tools.hal2doc
+    import cheap_pie.tools.search
+except:
+    import tools.hal2doc
+    import tools.search
