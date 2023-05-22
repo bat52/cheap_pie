5b8232c94e136062683aefa46a3d12dfbd0cb1f1
Marco Merlin <marcomerli@gmail.com>
2023-05-22 21:35:00 +0200

    remove pyverilator from auto import

--- src/cheap_pie/__init__.py
+++ src/cheap_pie/__init__.py
@@ -1,5 +1,5 @@
 """ Cheap Pie, a register and bitfield level tool for chip verification and validation """
 from .cheap_pie_core import *
-from .transport import *
 from .parsers import *
 from .tools import *
+from .transport import cp_dummy_transport
--- src/cheap_pie/cheap_pie_core/__init__.py
+++ src/cheap_pie/cheap_pie_core/__init__.py
@@ -1,4 +1,5 @@
 """ Core cheap pie classes """
 from .cp_hal import CpHal
 from .cp_builder import CpHalBuilder
-from .cp_cli import *
+from .cp_cli import cp_cli, cp_devices_fname, cp_devices_dir
+from .cheap_pie_main import cp_main
