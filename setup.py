5e0df3ffb30b6edc90f3157c65a29b548a6aa4f8
Marco Merlin <marcomerli@gmail.com>
2023-05-22 23:33:28 +0200

    added long description to pypi, version 1.0.0

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name='cheap_pie',
-    version='0.9.20',
+    version='1.0.0',
     license='Apache 2.0',
     author="Marco Merlin",
     author_email='marcomerli@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     description="A python tool for silicon validation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/bat52/cheap_pie',
     keywords='python silicon validation',
     install_requires=[
