9277213e2eed9bf81352fda0a43b3be503617943
Marco Merlin <marcomerli+github@gmail.com>
2024-04-29 21:16:33 +0200

    Update version 1.0.10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name='cheap_pie',
-    version='1.0.9',
+    version='1.0.10',
     license='Apache 2.0',
     author="Marco Merlin",
     author_email='marcomerli@gmail.com',
